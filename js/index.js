d95df3fadcb7b5f9e9a569602f31d6a938d5e331
iRogerz <rogertseng1025@gmail.com>
2020-12-01 14:42:59 +0800

    1.0
    做到更新熱門文章

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        prevArrow:'<span class="prev-arrow"><i class="fas fa-2x fa-caret-square-left"></i></span>',
+        nextArrow:'<span class="next-arrow"><i class="fas fa-2x fa-caret-square-right"></i></span>',
+        dots:true
+    });
+  });
